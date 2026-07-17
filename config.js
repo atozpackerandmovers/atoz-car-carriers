50ef98b58392a66c9fc49ae3271fab233cc418cd
atozpackerandmovers <manojgatibbi506@gmail.com>
2026-07-17 14:22:16 +0530

    Upload A TO Z Car Carriers website files

---
+++ config.js
@@ -0,0 +1,19 @@
+window.AZC_CONFIG = {
+  formspreeEndpoint: "https://formspree.io/f/mnnzokyb",
+  destinationEmail: "manojgatibbi506@gmail.com",
+  phoneNumber: "+91 9338888550",
+  whatsappNumber: "919338888550",
+  googleAdsConversionSendTo: "AW-CONVERSION_ID/CONVERSION_LABEL",
+  firebase: {
+    enabled: false,
+    collectionName: "enquiries",
+    config: {
+      apiKey: "",
+      authDomain: "",
+      projectId: "",
+      storageBucket: "",
+      messagingSenderId: "",
+      appId: ""
+    }
+  }
+};
